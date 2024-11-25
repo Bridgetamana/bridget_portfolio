6f0eccfc965e3f6a7cdef66be014382d05ded8ef
Bridget Amana <amanabridget03@gmail.com>
2024-11-25 16:16:13 +0100

    add toggle sound

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const circle = document.getElementById('circle');
 const body = document.body;
+var audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/click.mp3');
+var audio2 = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/clickUp.mp3');
 const savedTheme = localStorage.getItem('theme');
 
 function reveal() {
@@ -27,6 +29,12 @@ if (savedTheme) {
 circle.addEventListener('click', function () {
   const isDarkMode = body.classList.toggle('dark-mode');
 
+  if (isDarkMode) {
+    audio.play(); 
+  } else {
+    audio2.play(); 
+  }
+
   localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
 
   if (isDarkMode) {
@@ -35,6 +43,10 @@ circle.addEventListener('click', function () {
     circle.style.transform = 'rotate(0)';
   }
 });
+
+if (savedTheme === 'dark') {
+  body.classList.add('dark-mode');
+}
 document.addEventListener("DOMContentLoaded", function () {
   const progressBar = document.getElementById("progress-bar");
   const progressLabel = document.getElementById("progress-label");
